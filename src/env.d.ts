1e72e8a2c84180d90de9355f2d510658d5ab2afe
jennjunod <hello@teachjenntech.com>
2024-08-28 12:00:38 -0700

    learning python blog

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-    readonly CONTENTFUL_SPACE_ID: 5b5ossivsvn3;
-    readonly CONTENTFUL_DELIVERY_TOKEN: ZrBe0H3cOWAsMhvTOlWYuezZW-nCzsZ-jFPtFyS5iPs;
-    readonly CONTENTFUL_PREVIEW_TOKEN: hGWyzCt90R8iREQNrL4s8930Yv0xIfL5Q4vcCv3bTZ0;
-}
