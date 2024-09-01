76b1cd4643647bf8aa0c58ad78e18c532e1b1db2
jennjunod <hello@teachjenntech.com>
2024-09-01 15:04:02 -0700

    update share url image

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -2,6 +2,7 @@ import { Resvg, type ResvgRenderOptions } from '@resvg/resvg-js';
 import type { APIRoute } from 'astro';
 import satori from 'satori';
 import { html as toReactElement } from 'satori-html';
+import tjtLogo from '/src/assets/tjtLogo.png';
 
 const fontFile = await fetch(
   'https://og-playground.vercel.app/inter-latin-ext-700-normal.woff'
@@ -13,19 +14,19 @@ const height = 630;
 const width = 1200;
 
 export const GET: APIRoute = async () => {
-  const link = 'https://brutal.elian.codes';
+  const websiteLink = 'https://teachjenntech.com';
   const html = toReactElement(`
   <div style="background-color: white; display: flex; flex-direction: column; height: 100%; padding: 3rem; width: 100%">
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
-          <p style="font-size: 48px;">Brutal</p>
-          <p style="font-size: 38px;">A theme for Astro</p>
-          <p style="font-size: 38px;">Brought to you by Elian</p>
+          <p style="font-size: 48px;">Teach Jenn Tech</p>
+          <p style="font-size: 38px;">A website for tech learning and community</p>
+          <p style="font-size: 38px;">Hosted by Jenn Junod</p>
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
-          <p style="font-size: 32px">${link}</p>
-          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+          <p style="font-size: 32px">${websiteLink}</p>
+          <img src="${tjtLogo.src}" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
       </div>
     </div>
